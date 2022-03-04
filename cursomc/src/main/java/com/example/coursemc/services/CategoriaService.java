69b06484e97cd1e41f90aa6076c79b58aa225f5b
Marcovyski <marco_a_costa@hotmail.com>
2022-03-04 18:59:53 -0300

    Categorias endopoint adjustments/{id}

--- cursomc/src/main/java/com/example/coursemc/services/CategoriaService.java
+++ cursomc/src/main/java/com/example/coursemc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.coursemc.services;
 
+
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.coursemc.domain.Categoria;
 import com.example.coursemc.repositories.CategoriaRepository;
+import com.example.coursemc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {       
@@ -14,9 +16,9 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-
 }
